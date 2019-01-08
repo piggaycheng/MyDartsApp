66d3bc1359a535f9790b6e47212b8d1100780890
piggaycheng <piggaycheng123@gmail.com>
2019-01-08 21:49:02 +0800

    complete bottom navigation

---
+++ app/src/main/java/com/andy/mydartsapp/activity/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.andy.mydartsapp.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
+import com.andy.mydartsapp.R
+import com.andy.mydartsapp.fragment.HomeFragment
+import com.andy.mydartsapp.fragment.MoreFragment
+import com.andy.mydartsapp.fragment.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener = OnNavigationItemSelectedListener {item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                title = "Home"
+                val homeFragment = HomeFragment.newInstance()
+                openFragment(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile -> {
+                title = "Profile"
+                val prolfileFragment = ProfileFragment.newInstance()
+                openFragment(prolfileFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_more -> {
+                title = "More"
+                val moreFragment = MoreFragment.newInstance()
+                openFragment(moreFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            else -> {
+                return@OnNavigationItemSelectedListener false
+            }
+        }
+    }
+
+    private fun openFragment(fragment: android.support.v4.app.Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
---
+++ app/src/main/java/com/andy/mydartsapp/fragment/MoreFragment.kt
@@ -0,0 +1,80 @@
+package com.andy.mydartsapp.fragment
+
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import com.andy.mydartsapp.R
+import kotlinx.android.synthetic.main.fragment_more.*
+import kotlinx.android.synthetic.main.fragment_more.view.*
+import org.jetbrains.anko.support.v4.toast
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MoreFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class MoreFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView = inflater.inflate(R.layout.fragment_more, container, false)
+
+        rootView.btn_start_GB2.setOnClickListener(mOnClick)
+
+        return rootView
+    }
+
+    private val mOnClick = View.OnClickListener{
+        when(it.id) {
+            R.id.btn_start_GB2 -> {
+                toast("click GB2")
+            }
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MoreFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            MoreFragment().apply {
+                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
